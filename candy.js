10c5246acab6124130848d0f5c249733e5e539c7
Justin <justinkwanchan@gmail.com>
2021-06-28 22:37:00 -0400

    Complete candy

--- candy.js
+++ candy.js
@@ -1,5 +1,4 @@
 // There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
-
 // You are giving candies to these children subjected to the following requirements:
 
 // Each child must have at least one candy.
@@ -17,6 +16,12 @@
 // Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 // The third child gets 1 candy because it satisfies the above two conditions.
 
+// Input: ratings = [1,2,3] => [1,2,3]
+// Output: 6
+
+// Input: ratings = [3,2,1] => [3,2,1]
+// Output: 6
+
 // Constraints:
 
 // n == ratings.length
@@ -27,6 +32,28 @@
  * @param {number[]} ratings
  * @return {number}
  */
-var candy = function (ratings) {};
+var candy = function (ratings) {
+  const candy = [...Array(ratings.length)].map(() => 1);
 
-module.exports(candy);
+  const growBack = i => {
+    candy[i]++;
+
+    if (ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i]) {
+      growBack(i - 1);
+    }
+  };
+
+  ratings.forEach((rating, i) => {
+    if (rating > ratings[i - 1]) {
+      candy[i] = candy[i - 1] + 1;
+    }
+
+    if (rating < ratings[i - 1] && candy[i - 1] <= candy[i]) {
+      growBack(i - 1);
+    }
+  });
+
+  return candy.reduce((acc, cur) => acc + cur, 0);
+};
+
+module.exports = candy;
--- test/candy-test.js
+++ test/candy-test.js
@@ -1,10 +1,29 @@
-const assert = require('assert');
+const { assert } = require('chai');
 const candy = require('../candy');
 
 describe('candy', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+  it('should return 4 for [1, 2, 1]', function () {
+    const result = candy([1, 2, 1]);
+    assert.equal(result, 4);
+  });
+
+  it('should return 5 for [1, 0, 2]', function () {
+    const result = candy([1, 0, 2]);
+    assert.equal(result, 5);
+  });
+
+  it('should return 6 for [1, 2, 3]', function () {
+    const result = candy([1, 2, 3]);
+    assert.equal(result, 6);
+  });
+
+  it('should return 6 for [3, 2, 1]', function () {
+    const result = candy([3, 2, 1]);
+    assert.equal(result, 6);
+  });
+
+  it('should return 13 for [1, 1, 2, 1, 2, 2, 3, 3, 1]', function () {
+    const result = candy([1, 1, 2, 1, 2, 2, 3, 3, 1]);
+    assert.equal(result, 13);
   });
 });
