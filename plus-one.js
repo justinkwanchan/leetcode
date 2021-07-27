ec5551ddfbd3de55fe1ba36e84735eb4d2c569f8
Justin <justinkwanchan@gmail.com>
2021-07-27 05:54:41 -0400

    Refactor solution

--- plus-one.js
+++ plus-one.js
@@ -32,15 +32,13 @@ var plusOne = function (digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] !== 9) {
       digits[i]++;
-      break;
+      return digits;
     }
 
     digits[i] = 0;
-
-    if (i === 0) {
-      digits.unshift(1);
-    }
   }
+
+  digits.unshift(1);
 
   return digits;
 };
