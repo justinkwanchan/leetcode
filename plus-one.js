3953baa21c7ee32896cb2bbfa8506cd9d030166b
Justin <justinkwanchan@gmail.com>
2021-07-27 05:48:31 -0400

    Implement mutating array solution to plus-one

--- plus-one.js
+++ plus-one.js
@@ -24,8 +24,25 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+// var plusOne = function (digits) {
+//   return (BigInt(digits.join('')) + BigInt(1)).toString().split('');
+// };
+
 var plusOne = function (digits) {
-  return (BigInt(digits.join('')) + BigInt(1)).toString().split('');
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      break;
+    }
+
+    digits[i] = 0;
+
+    if (i === 0) {
+      digits.unshift(1);
+    }
+  }
+
+  return digits;
 };
 
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+console.log(plusOne([9, 9]));
